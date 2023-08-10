6bc75d9870599e55db93eaa304b0c7952985018c
FernandoDCardozoBraga <fer.car.br@gmail.com>
2023-08-10 10:10:46 -0300

    html added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-/***Sticky navbar***/
+/***Scroll***/
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('.nav_bar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.nav_bar a[href*=' + id +']').classList.add('active')
+            });
+        };
+    });
+}
